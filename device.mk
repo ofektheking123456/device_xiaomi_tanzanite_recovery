0dec0b64eaa320590b0722e3ab10e6f08f6cb87b
picasso09 <picasso09DV@proton.me>
2025-03-01 09:45:49 +0700

    tanzanite: include keystore hal package
    Signed-off-by: picasso09 <picasso09DV@proton.me>
    
    Date:      Sat Mar 1 09:45:49 2025 +0700
    Change-Id: 23fec173836c2c3bdaf508981a1d73dfa4bfbfa0

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils.recovery
 
+# Keystore Hal
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1
